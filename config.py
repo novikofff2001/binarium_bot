9aff6e276d80ba6509bb43380762e1b700c90d96
Nikita Novikov <nikita.novikov.04.12.2001@gmail.com>
2021-09-17 17:33:41 +0300

    Added token validation

--- config.py
+++ config.py
@@ -1,5 +1,22 @@
-TOKEN = open("token.txt", 'r').read()
-ADMINISTRATORS = open("admins.txt", 'r').readlines()
+import os.path
+#
+TOKEN_PATH = "token.txt"
+TOKEN = str()
+if os.path.exists(TOKEN_PATH):
+    TOKEN = open(TOKEN_PATH, 'r').read()
+else:
+    open(TOKEN_PATH, 'w').close()
+    raise Exception("Put bot's token in {0} file!".format(TOKEN_PATH))
+#
+ADMINISTRATORS_PATH = "admins.txt"
+ADMINISTRATORS = set()
+if os.path.exists(ADMINISTRATORS_PATH):
+    for elem in open(ADMINISTRATORS_PATH, 'r').readlines():
+        ADMINISTRATORS.add(int(elem))
+else:
+    open(ADMINISTRATORS_PATH, 'w').close()
+if len(ADMINISTRATORS) == 0:
+    print("WARN: There is no administrators at this bot")
 #
 USERS_PATH = 'users/'
 ICONS_PATH = 'icons/'
