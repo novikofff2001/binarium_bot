6743883042833ec9e2a7e1f73a149f6d1a539dba
Nikita Novikov <nikita.novikov.04.12.2001@gmail.com>
2021-09-17 16:29:30 +0300

    Refactored configurations

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = open("token.txt", 'r').read()
+ADMINISTRATORS = open("admins.txt", 'r').readlines()
+#
+USERS_PATH = 'users/'
+ICONS_PATH = 'icons/'
+WEBDRIVER_PATH = 'webdriver/chromedriver.exe'
+ALL_OPTIONS_PATH = 'database/all_options.txt'
+ALL_TIMES_PATH = 'database/all_times.txt'
+#
+URL = 'https://binarium.global'
+WELCOME_MESSAGE = """Welcome to Binarium bot! Set login and password using /login and /pass"""
